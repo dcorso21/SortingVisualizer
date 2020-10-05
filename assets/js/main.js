8fd40c05b740a23709a48475096e23841032d906
dcorso21 <david.l.corson@gmail.com>
2020-10-05 01:04:55 -0400

    animations working left to right

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 let [num1, num4, num3, num2] = document.querySelectorAll(".num");
-let tl = anime.timeline()
+let tl = anime.timeline();
 
 window.onload = () => {
-    firstScan();
-    moveE1(num4, num2);
-    moveE2(num4, num2);
+    // firstScan();
+    // moveE1(num4, num2);
+    // moveE2(num4, num2);
     endScan();
 };
 
@@ -15,8 +15,8 @@ function firstScan() {
         tl.add({
             targets: seq[i],
             keyframes: [
-                {backgroundColor: color},
-                {backgroundColor: "#fff"},
+                { backgroundColor: color },
+                { backgroundColor: "#fff" },
             ],
             duration: 700,
             easing: "linear",
@@ -29,7 +29,7 @@ function endScan() {
     tl.add({
         targets: [num1, num2, num3, num4],
         backgroundColor: ["#fff", "#afea97"],
-        duration: 300*2,
+        duration: 300 * 2,
         easing: "linear",
         delay: anime.stagger(300),
         direction: "alternate",
@@ -61,14 +61,104 @@ function moveE1(e1, e2) {
 }
 
 function moveE2(e1, e2) {
-    tl.add({
-        targets: e2,
-        keyframes: [
-            { translateY: 40 },
-            { translateX: getRelativeX(e2, e1) },
-            { translateY: 0 },
-        ],
-        duration: 2500,
-        easing: "easeOutElastic(1, .8)",
-    }, "-=2500");
+    tl.add(
+        {
+            targets: e2,
+            keyframes: [
+                { translateY: 40 },
+                { translateX: getRelativeX(e2, e1) },
+                { translateY: 0 },
+            ],
+            duration: 2500,
+            easing: "easeOutElastic(1, .8)",
+        },
+        "-=2500"
+    );
 }
+
+let nums = [num1, num2, num3, num4];
+let brackets = document.querySelectorAll(".bracket");
+
+let dragVal,
+    dragapprove = true;
+nums.map((num) => {
+    num.ondragstart = (e) => {
+        // let arr = document.getElementsByClassName("arr")[0];
+        requestAnimationFrame(() => {
+            e.target.classList.add("hide-while-dragged");
+            // e.dataTransfer.setData("text", e.target.id)
+            dragVal = e.target;
+        });
+    };
+    num.ondragend = (e) => {
+        e.srcElement.classList.remove("hide-while-dragged");
+    };
+
+    num.ondragover = (e) => {
+        if (!dragapprove) return;
+        dragapprove = false;
+        e.preventDefault();
+        let nodes = e.path[1].childNodes,
+            numOrd = [];
+
+        for (let i = 0; i < nodes.length; i++) {
+            if (nums.includes(nodes[i])) {
+                numOrd.push(nodes[i]);
+            }
+        }
+
+        let gap = numOrd.indexOf(dragVal) + 1,
+            hovering = numOrd.indexOf(e.target) + 1,
+            before = gap === 1 ? [] : numOrd.slice(0, Math.min(gap, hovering)),
+            toAnimate = numOrd.slice(
+                Math.min(gap, hovering),
+                Math.max(gap, hovering)
+            ),
+            after =
+                gap === numOrd.length
+                    ? []
+                    : numOrd.slice(Math.max(gap, hovering)),
+            xdist;
+
+        // console.log("b", before, "anim", toAnimate, "a", after);
+
+        for (let i = 0; i < toAnimate.length; i++) {
+            if (i === 0) {
+                xdist = getRelativeX(toAnimate[i], dragVal) + 10000;
+            }
+            anime({
+                targets: toAnimate[i],
+                translateX: xdist,
+                duration: 175,
+                easing: "easeOutElastic(1, .8)",
+            });
+        }
+        let newArray = before.concat(toAnimate);
+        newArray.push(dragVal);
+        newArray = newArray.concat(after);
+        setTimeout(() => {
+            newArray.map((el) => {
+                if (el !== dragVal) {
+                    el.style.transform = "none";
+                }
+            });
+            let arr = e.path[1];
+            while (arr.firstChild){
+                arr.removeChild(arr.firstChild)
+            }
+
+            newArray.unshift(brackets[0])
+            newArray.push(brackets[1])
+            
+            newArray.map((el) => {
+                arr.appendChild(el)
+            })
+            dragapprove = true
+            // console.log(newArray);
+        }, 200);
+    };
+    num.ondrop = (e) => {
+        e.preventDefault();
+        // e.target.appendChild(draggedIcon);
+    };
+});
